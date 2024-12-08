dd6414e37a657a8f12c153dbfab90e575eac2c08
dan <dan6661.accounts@proton.me>
2024-12-08 11:06:47 +0100

    fixed CORS issues

--- app.py
+++ app.py
@@ -1,66 +1,57 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
-    app.config['CORS_HEADERS'] = 'Content-Type'
     
-    # import controllers
+    # Enable CORS globally
+    CORS(app, resources={r"/*": {"origins": "*"}})
+
+    # Import controllers
     from controllers.MarketController import MarketController
 
-    # TODO this needs to get reorganized
+    # Instantiate controller
     market_controller = MarketController()
-    
+
     @app.route('/markets')
-    @cross_origin()
     def market_data():
-        # get the custom tickers from the request URL parameters
+        # Get the custom tickers from the request URL parameters
         tickers = request.args.getlist('tickers')
 
-        # if tickers are not provided, use the default list
+        # If tickers are not provided, use the default list
         if not tickers:
             print('No tickers specified. Using default...')
             tickers = ['^GSPC', '^GSPTSE', 'GC=F', 'CL=F', 'BTC-USD']
 
-        # get the market data
+        # Get the market data
         market_data = market_controller.get_market_data(tickers)
 
         return jsonify(market_data)
 
-            
     @app.route('/markets/<ticker>')
-    @cross_origin()
     def single_ticker_data(ticker):
         ticker_data = market_controller.get_single_ticker(ticker)
         return jsonify(ticker_data)
-    
+
     @app.route('/dummy_markets')
-    @cross_origin()
     def dummy_data():
         dummy_data = market_controller.get_dummy_data()
         return jsonify(dummy_data)
 
-    # test route
+    # Test route
     @app.route('/')
     def hello_world():
         return "Izzy is pretty"
 
-    # error handler
+    # Error handler
     @app.errorhandler(404)
     def not_found(error):
         return jsonify({"error": "Not Found"}), 404
 
     return app
 
+# Create the app
 app = create_app()
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-
-@app.after_request
-def add_cors_headers(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
 
 if __name__ == '__main__':
     app.run()
